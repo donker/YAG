a151ad058059bf0635917aeba41635e48818b8a6
donker <none@none.com>
2011-05-27 11:02:47 +0000

    First code checkin

---
+++ Templates/Slimbox2/jsblocks/block.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+ $('#lb[settings:moduleid] a').slimbox({
+loop: [settings:loop],
+overlayOpacity: [settings:overlayOpacity],
+overlayFadeDuration: [settings:overlayFadeDuration],
+resizeDuration: [settings:resizeDuration],
+resizeEasing: "[settings:resizeEasing]",
+initialWidth: [settings:initialWidth],
+initialHeight: [settings:initialHeight],
+imageFadeDuration: [settings:imageFadeDuration],
+captionAnimationDuration: [settings:captionAnimationDuration],
+counterText: "[resx:counterText]"
+ });
+});

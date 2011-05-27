1c57797c1b25dd719f0ba2e79ff90a488f974ad3
donker <none@none.com>
2011-05-27 11:02:47 +0000

    First code checkin

---
+++ Templates/Cycle/jsblocks/block.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+ $('#p[settings:moduleid]').cycle({ fx: '[settings:fx]',
+ autostop: [settings:autostop],
+ autostopCount: [settings:autostopCount],
+ backwards: [settings:backwards],
+ cleartypeNoBg: [settings:cleartypeNoBg],
+ containerResize: [settings:containerResize],
+ continuous: [settings:continuous],
+ delay: [settings:delay],
+ fastOnEvent: [settings:fastOnEvent],
+ fit: [settings:fit],
+ height: '[settings:height]',
+ manualTrump: [settings:manualTrump],
+ nowrap: [settings:nowrap],
+ pause: [settings:pause],
+ random: [settings:random],
+ randomizeEffects: [settings:randomizeEffects],
+ requeueOnImageNotLoaded: [settings:requeueOnImageNotLoaded],
+ requeueTimeout: [settings:requeueTimeout],
+ rev: [settings:rev],
+ slideResize: [settings:slideResize],
+ speed: [settings:speed],
+ sync: [settings:sync],
+ timeout: [settings:timeout]
+ });
+});

a6d004bd63e700b558bafabee67f845503e65953
donker <none@none.com>
2011-05-27 11:02:47 +0000

    First code checkin

---
+++ Templates/_default/jsblocks/block.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+ $("#g_[settings:moduleid]:first a\[rel^='prettyPhoto'\]").prettyPhoto({ animation_speed: '[settings:animation_speed]', theme: '[settings:theme]', slideshow: [settings:slideshow], autoplay_slideshow: [settings:autoplay_slideshow], opacity: [settings:opacity], show_title: [settings:show_title], allow_resize: [settings:allow_resize], horizontal_padding: [settings:horizontal_padding], autoplay: [settings:autoplay], modal: [settings:modal], overlay_gallery: [settings:overlay_gallery], keyboard_shortcuts: [settings:keyboard_shortcuts] });
+});

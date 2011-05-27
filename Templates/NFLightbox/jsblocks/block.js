d756964b2ff4914c2cc1692cbdf6a4bd5e01e614
donker <none@none.com>
2011-05-27 11:02:47 +0000

    First code checkin

---
+++ Templates/NFLightbox/jsblocks/block.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+ var settings = {
+  overlayBgColor: '[settings:overlayBgColor]',
+  overlayOpacity: 0.[settings:overlayOpacity],
+  fixedNavigation: [settings:fixedNavigation],
+  imageLoading: '[settings:templatepath]/[settings:imageLoading]',
+  imageBtnPrev: '[settings:templatepath]/[settings:imageBtnPrev]',
+  imageBtnNext: '[settings:templatepath]/[settings:imageBtnNext]',
+  imageBtnClose: '[settings:templatepath]/[settings:imageBtnClose]',
+  imageBlank: '[settings:templatepath]/[settings:imageBlank]',
+  imageBtnBottomPrev: '[settings:templatepath]/[settings:imageBtnBottomPrev]',
+  imageBtnBottomNext: '[settings:templatepath]/[settings:imageBtnBottomNext]',
+  imageBtnPlay: '[settings:templatepath]/[settings:imageBtnPlay]',
+  imageBtnStop: '[settings:templatepath]/[settings:imageBtnStop]',
+  containerBorderSize: [settings:containerBorderSize],
+  containerResizeSpeed: [settings:containerResizeSpeed],
+  txtImage: '[resx:txtImage.Text]',
+  txtOf: '[resx:txtOf.Text]',
+  txtPrev: '[resx:txtPrev.Text]',
+  txtNext: '[resx:txtNext.Text]'
+ };
+ $('#gallery[settings:moduleid] a').lightBox(settings);
+});

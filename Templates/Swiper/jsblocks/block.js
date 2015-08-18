7f06c932b72f8353048389f8ad012f0b61133589
Peter Donker <peter@bring2mind.net>
2015-08-18 20:41:08 +0200

    Added Swiper template

---
+++ Templates/Swiper/jsblocks/block.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	var mySwiper = new Swiper('.swiper-container', {
+		pagination: ".swiper-pagination",
+		paginationClickable: true,
+		nextButton: ".swiper-button-next",
+		prevButton: ".swiper-button-prev",
+		autoplay: [settings:autoplay],
+		loop: true,
+		effect: '[settings:effect]'
+	})
+});

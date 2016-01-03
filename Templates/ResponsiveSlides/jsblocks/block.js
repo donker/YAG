e5e964caf336e275e271760a8a0859f3a2a17025
Peter Donker <peter@bring2mind.net>
2016-01-03 15:01:11 +0100

    New template: ResponsiveSlides

---
+++ Templates/ResponsiveSlides/jsblocks/block.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+  $('#p[settings:moduleid]').responsiveSlides({
+    auto: [settings:auto],
+    speed: [settings:speed],
+    timeout: [settings:timeout],
+    pager: [settings:pager],
+    nav: [settings:nav],
+    random: [settings:random],
+    pause: [settings:pause],
+    pauseControls: [settings:pauseControls],
+    prevText: '[settings:prevText]',
+    nextText: '[settings:nextText]',
+    maxwidth: '[settings:maxwidth]'
+  });
+});

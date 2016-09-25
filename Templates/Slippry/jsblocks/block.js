1752908e4da887ee8569efcdbd3fd3d92ed5c6d4
Peter Donker <peter@bring2mind.net>
2016-09-25 19:43:30 +0200

    New template: Slippry

---
+++ Templates/Slippry/jsblocks/block.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+  $('#slippry[settings:moduleid] a[href=""]').click(function(e) {
+    e.preventDefault();
+  });
+  $('#slippry[settings:moduleid]').slippry({
+    captions: '[settings:captions]'=='none'?false:'[settings:captions]',
+    responsive: [settings:responsive],
+    pager: [settings:pager],
+    controls: [settings:controls],
+    speed: [settings:speed],
+    pause: [settings:pause],
+    transition: '[settings:transition]'=='none'?false:'[settings:transition]'
+  });
+});
